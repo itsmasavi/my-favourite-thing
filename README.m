6dd9847005ee0aa76fb9e646efa57bc4062b1772
Collins Masavi <masavicollins@gmail.com>
2021-06-02 15:36:50 +0300

    css styles added

---
+++ README.m
@@ -0,0 +1,10 @@
+# {My Favourite Thing}
+#### {This is a website about my favourite team manud}, {01/may/2021}
+#### By **{Collins Masavi}**
+## Description
+{This website shows the current logo for the team ,a number of legends and also the players }
+## Setup/Installation Requirements
+just run it on any web browser
+### License
+{MIT.}
+Copyright (c) {2021} **{Collins Masavi}**
